fe9e6b3bfe49cc282db770950babfd23266c58ec
con-qr <62906344+con-qr@users.noreply.github.com>
2020-04-02 13:59:36 -0300

    Add files via upload

---
+++ precache-manifest.51b9204589870489338dd589e4a78b28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6352062516636f12ef40552c1e064634",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "50958922f856acbf1921",
+    "url": "/app/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "c3fcf10240d9d3deaea6",
+    "url": "/app/static/js/2.c316e73e.chunk.js"
+  },
+  {
+    "revision": "a1212b395e88f070a42368ea2ff93e06",
+    "url": "/app/static/js/2.c316e73e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50958922f856acbf1921",
+    "url": "/app/static/js/main.9a4c6de7.chunk.js"
+  },
+  {
+    "revision": "c891c10f8b091a81bc2c",
+    "url": "/app/static/js/runtime-main.4944cc88.js"
+  }
+]);
